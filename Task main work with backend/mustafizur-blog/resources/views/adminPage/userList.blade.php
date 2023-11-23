c9b8d0732183f46b0c55b9a823fc8c1e0dc5ae29
sabbirpegon <sabbirwalton343@gmail.com>
2023-11-24 01:12:06 +0600

    user list design done

--- Task main work with backend/mustafizur-blog/resources/views/adminPage/userList.blade.php
+++ Task main work with backend/mustafizur-blog/resources/views/adminPage/userList.blade.php
@@ -5,13 +5,77 @@
         </h2>
     </x-slot>
 
+
+
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-700 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                    SL
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-700 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                    Image
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-700 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                    Name
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-700 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                    Role
+                                </th>
+                                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-700 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Example row -->
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">1</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex-shrink-0">
+                                        <img src="https://i.pinimg.com/236x/d9/f0/c9/d9f0c959c093dc1bf714f39570418f48.jpg" alt="Image Alt Text" class="h-12 w-12 rounded-full">
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">John Doe</td>
+                                <td class="px-6 py-4 whitespace-nowrap">Admin</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                    <!-- <a href="#" class="text-red-600 hover:text-red-900 ml-2">Delete</a> -->
+                                </td>
+                            </tr>
+                            <!-- Add more rows as needed -->
+                            <!-- Another example row with a line -->
+                            <tr>
+                                <td class="border-t border-gray-200" colspan="5"></td>
+                            </tr>
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">2</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex-shrink-0">
+                                        <img src="https://i.pinimg.com/236x/d9/f0/c9/d9f0c959c093dc1bf714f39570418f48.jpg" alt="Image Alt Text 2" class="h-12 w-12 rounded-full">
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">Jane Doe</td>
+                                <td class="px-6 py-4 whitespace-nowrap">User</td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                    <!-- <a href="#" class="text-red-600 hover:text-red-900 ml-2">Delete</a> -->
+                                </td>
+                            </tr>
+                            <!-- Add more rows as needed -->
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+
+
+
+
 </x-app-layout>
